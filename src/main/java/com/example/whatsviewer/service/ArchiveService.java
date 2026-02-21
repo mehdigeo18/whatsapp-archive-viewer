f309059240a9d65c7f9bcf84971757ef344bc637
mehdigeo18 <mehdigeo@gmail.com>
2026-02-21 17:00:58 +0400

    Delete src/main directory

--- src/main/java/com/example/whatsviewer/service/ArchiveService.java
+++
@@ -1,8 +0,0 @@
-package com.example.whatsviewer.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ArchiveService {
-  // Reserved for future features (search, filters, pagination, etc.)
-}
