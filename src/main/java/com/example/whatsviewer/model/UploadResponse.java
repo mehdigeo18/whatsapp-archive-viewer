d00f7ec8d3ae08b9c47041ee6b96fbe24f7535fb
mehdigeo18 <mehdigeo@gmail.com>
2026-02-21 17:57:47 +0400

    Update UploadResponse.java

--- src/main/java/com/example/whatsviewer/model/UploadResponse.java
+++ src/main/java/com/example/whatsviewer/model/UploadResponse.java
@@ -3,50 +3,29 @@ package com.example.whatsviewer.model;
 import java.util.List;
 
 public class UploadResponse {
-  private String chatName;
-  private String myName;
-  private int totalMessages;
-  private List<ChatMessage> messages;
 
-  public UploadResponse() {}
+    private String chatName;
+    private String myName;
+    private int totalMessages;
+    private List<ChatMessage> messages;
 
-  public UploadResponse(String chatName, String myName, List<ChatMessage> messages) {
-    this.chatName = chatName;
-    this.myName = myName;
-    this.messages = messages;
-    this.totalMessages = messages == null ? 0 : messages.size();
-  }
+    // IMPORTANT: used for /api/media/{uploadId}?name=...
+    private String uploadId;
 
-  public String getChatName() {
-    return chatName;
-  }
+    public UploadResponse() {}
 
-  public void setChatName(String chatName) {
-    this.chatName = chatName;
-  }
+    public String getChatName() { return chatName; }
+    public void setChatName(String chatName) { this.chatName = chatName; }
 
-  public String getMyName() {
-    return myName;
-  }
+    public String getMyName() { return myName; }
+    public void setMyName(String myName) { this.myName = myName; }
 
-  public void setMyName(String myName) {
-    this.myName = myName;
-  }
+    public int getTotalMessages() { return totalMessages; }
+    public void setTotalMessages(int totalMessages) { this.totalMessages = totalMessages; }
 
-  public int getTotalMessages() {
-    return totalMessages;
-  }
+    public List<ChatMessage> getMessages() { return messages; }
+    public void setMessages(List<ChatMessage> messages) { this.messages = messages; }
 
-  public void setTotalMessages(int totalMessages) {
-    this.totalMessages = totalMessages;
-  }
-
-  public List<ChatMessage> getMessages() {
-    return messages;
-  }
-
-  public void setMessages(List<ChatMessage> messages) {
-    this.messages = messages;
-    this.totalMessages = messages == null ? 0 : messages.size();
-  }
+    public String getUploadId() { return uploadId; }
+    public void setUploadId(String uploadId) { this.uploadId = uploadId; }
 }
